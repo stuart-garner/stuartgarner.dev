bfc1d2d87246ea944a3dd64783eb86371a309942
Stuart Garner <sgarner4@networkrail.co.uk>
2023-01-17 10:55:16 +0000

    added mobile menu

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -2,11 +2,12 @@ import { useEffect, useState } from "react";
 
 const Header = () => {
   const [show, setShow] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
 
   useEffect(() => {
     const controlNavbar = () => {
       if (typeof window !== "undefined") {
-        window.scrollY > 50 ? setShow(true) : setShow(false);
+        window.scrollY > 175 ? setShow(true) : setShow(false);
       }
     };
 
@@ -18,12 +19,41 @@ const Header = () => {
     }
   }, []);
 
+  const onMenuToggle = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
-    <header className={`navBar ${show && "show"} fixed z-[9999] `}>
-      <div className={`container flex h-full items-center bg-white `}>
-        <h1 className="m-0 align-middle text-xl text-gray-800">[SG]</h1>
-      </div>
-    </header>
+    <>
+      <header className={`nav-bar ${show && "show"}`}>
+        <h1 className="m-0 align-middle text-xl ">[SG]</h1>
+        <div id="menuToggle" className="hidden">
+          Burger Menu
+        </div>
+        <div className="menu-toggle">
+          <input
+            type="checkbox"
+            aria-labelledby="menuToggle"
+            onChange={onMenuToggle}
+          />
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </header>
+      <nav
+        className={`mobile-menu ${showMenu && "show-menu"}`}
+        role="navigation"
+      >
+        <ul id="menu">
+          <li>Home</li>
+          <li>About</li>
+          <li>Info</li>
+          <li>Contact</li>
+          <li>Show me more</li>
+        </ul>
+      </nav>
+    </>
   );
 };
 
