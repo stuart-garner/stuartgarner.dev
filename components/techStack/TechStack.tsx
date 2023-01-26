098a00606b8c86be6e3c7372ac67ffd424c1b00e
Stuart Garner <sgarner4@networkrail.co.uk>
2023-01-26 15:43:53 +0000

    Fixed heading tags

--- components/techStack/TechStack.tsx
+++ components/techStack/TechStack.tsx
@@ -11,16 +11,16 @@ function TechStack() {
     <div className="container w-full pt-[50px]">
       <div className="flex w-full flex-row justify-between gap-5 text-gray-400 md:justify-center md:gap-20 xl:gap-5">
         <span className="flex flex-col items-center gap-5 xl:flex-row">
-          <h6>Built with:</h6>
+          <h5>Built with:</h5>
           <Next style="tech-stack-logo" />
           <TypeScript style="tech-stack-logo" />
           <Tailwind style="tech-stack-logo" />
         </span>
         <span className="flex flex-col items-center gap-5 xl:flex-row">
-          <h6>Powered by:</h6>
+          <h5>Powered by:</h5>
           <Prismic style="tech-stack-logo" />
           <Formspree style="tech-stack-logo" />
-          <h6>Hosted by:</h6>
+          <h5>Hosted by:</h5>
           <Vercel style="tech-stack-logo" />
         </span>
       </div>
